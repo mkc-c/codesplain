239c7992ab5cb5f0bfcd5ae30eb63387f120f289
mkc-c <abdirisak.arr@gmail.com>
2024-01-20 10:06:51 +0000

    add console.log to find required property for practice bug fix

--- src/components/repositories/RepositoriesListItem.js
+++ src/components/repositories/RepositoriesListItem.js
@@ -1,9 +1,11 @@
-import { Link } from 'react-router-dom';
-import FileIcon from '../tree/FileIcon';
-import RepositoriesSummary from './RepositoriesSummary';
+import { Link } from "react-router-dom";
+import FileIcon from "../tree/FileIcon";
+import RepositoriesSummary from "./RepositoriesSummary";
 
 function RepositoriesListItem({ repository }) {
   const { full_name, language, description, owner, name } = repository;
+
+  console.log(repository);
 
   return (
     <div className="py-3 border-b flex">
