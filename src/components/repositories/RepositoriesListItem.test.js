4ad0e53e60754fdc833ae49b40708ab5e2cdac21
mkc-c <abdirisak.arr@gmail.com>
2024-01-20 10:23:15 +0000

    mock repository prop object and pass to component render

--- src/components/repositories/RepositoriesListItem.test.js
+++ src/components/repositories/RepositoriesListItem.test.js
@@ -2,7 +2,15 @@ import { render, screen } from "@testing-library/react";
 import RepositoriesListItem from "./RepositoriesListItem";
 
 function renderComponent() {
-  render(<RepositoriesListItem />);
+  const repository = {
+    full_name: "facebook/react",
+    language: "Javascript",
+    description: "A js library",
+    owner: "facebook",
+    name: "react",
+    html_url: "https://github.com/facebook/react",
+  };
+  render(<RepositoriesListItem repository={repository} />);
 }
 
 test("shows a link to the github homepage for this repository", () => {
