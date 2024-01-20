ded206077f459da1113d404bac8714e0c4814600
mkc-c <abdirisak.arr@gmail.com>
2024-01-20 12:50:45 +0000

    add option of using act with pause function to allow for time for data fetching within nested component

--- src/components/repositories/RepositoriesListItem.test.js
+++ src/components/repositories/RepositoriesListItem.test.js
@@ -1,12 +1,12 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, act } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
 import RepositoriesListItem from "./RepositoriesListItem";
 
-jest.mock("../tree/FileIcon.js", () => {
-  return () => {
-    return "File Icon Component";
-  };
-});
+// jest.mock("../tree/FileIcon.js", () => {
+//   return () => {
+//     return "File Icon Component";
+//   };
+// });
 
 function renderComponent() {
   const repository = {
@@ -26,6 +26,10 @@ function renderComponent() {
 
 test("shows a link to the github homepage for this repository", async () => {
   renderComponent();
+
+  await act(async () => {
+    await pause();
+  });
 
   //   await screen.findByRole("img", {
   //     name: "Javascript",
